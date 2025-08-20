3969e044c96607301d26c67eb5f85229d58227c2
zivtal <zivtal83@gmail.com>
2025-08-20 08:04:22 +0300

    Add immediate redirect from Home to Instance page
    - Use `useEffect` with `useNavigate` to redirect to `/instance` upon visiting Home.

--- src/client/pages/Home/Home.tsx
+++ src/client/pages/Home/Home.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
-  return <></>;
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Immediate redirect
+    navigate('/instance', { replace: true });
+  }, [navigate]);
+
+  return <></>; // or a loading spinner
 };
 
 export default Home;
