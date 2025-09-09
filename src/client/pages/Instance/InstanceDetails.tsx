4a0ba0542e4be825e2490e19d82207501d1d4fee
zivtal <zivtal83@gmail.com>
2025-09-09 10:10:54 +0300

    feat(InstanceDetails) add back navigation button to instance details view

--- src/client/pages/Instance/InstanceDetails.tsx
+++ src/client/pages/Instance/InstanceDetails.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import Button from '@client/shared/components/Button/Button';
 
 type Props = {
   phoneNumber: string;
 };
 
 const InstanceDetails = ({ phoneNumber }: Props) => {
-  return <div>{phoneNumber}</div>;
+  const navigate = useNavigate();
+
+  const handleBack = () => {
+    navigate('/instance');
+  };
+
+  return (
+    <div className="p-4">
+      <div className="mb-4">
+        <Button buttonType="flat" onClick={handleBack}>
+          ← Back to Instances
+        </Button>
+      </div>
+      <div>{phoneNumber}</div>
+    </div>
+  );
 };
 
 export default InstanceDetails;
