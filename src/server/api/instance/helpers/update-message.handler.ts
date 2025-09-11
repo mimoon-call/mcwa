320b8864c6d685b19bd59644d33fa8367058571a
zivtal <zivtal83@gmail.com>
2025-09-11 23:57:58 +0300

    refactor(updateMessageHandler) simplify update logic by consolidating parameters

--- src/server/api/instance/helpers/update-message.handler.ts
+++ src/server/api/instance/helpers/update-message.handler.ts
@@ -5,29 +5,17 @@ import { app } from '@server/index';
 
 const updateTimeout = new Map<string, NodeJS.Timeout>();
 
-export const updateMessageHandler: WAMessageUpdateCallback = async (
-  messageId,
-  { status, sentAt, deliveredAt, readAt, playedAt, errorMessage, errorCode }
-) => {
+export const updateMessageHandler: WAMessageUpdateCallback = async (messageId, data) => {
   clearTimeout(updateTimeout.get(messageId));
 
   updateTimeout.set(
     messageId,
     setTimeout(async () => {
-      await WhatsAppMessage.updateOne({ messageId }, { $set: { status, sentAt, deliveredAt, readAt, playedAt, errorMessage, errorCode } });
+      await WhatsAppMessage.updateOne({ messageId }, { $set: data });
       updateTimeout.delete(messageId);
     }, 2000)
   );
 
   // Broadcast status update to connected clients
-  app.socket.broadcast(ConversationEventEnum.MESSAGE_STATUS_UPDATE, {
-    messageId,
-    status,
-    sentAt,
-    deliveredAt,
-    readAt,
-    playedAt,
-    errorCode,
-    errorMessage,
-  });
+  app.socket.broadcast(ConversationEventEnum.MESSAGE_STATUS_UPDATE, { ...data, messageId });
 };
