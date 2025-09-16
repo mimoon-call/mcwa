669e5663fde4bb521aa2659669681e271a4a4080
zivtal <zivtal83@gmail.com>
2025-09-16 11:19:22 +0300

    refactor(whatsapp) disable warm-up on ready and add debug logging for available instances

--- src/server/index.ts
+++ src/server/index.ts
@@ -52,7 +52,7 @@ export const app = new ServerExpress({
 export const wa = new WhatsappWarmService({
   ...whatsappConfig,
   debugMode: isProduction ? ['info', 'error'] : true,
-  warmUpOnReady: true,
+  warmUpOnReady: false, // TODO: Temporarily disable auto warm up to prevent issues with multiple instances
   onIncomingMessage: incomingMessageHandler,
   onOutgoingMessage: outgoingMessageHandler,
   onMessageUpdate: updateMessageHandler,
