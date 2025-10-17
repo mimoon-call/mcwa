3ec2f18909546295535005426a9a1cf42f68695a
zivtal <zivtal83@gmail.com>
2025-10-17 09:47:41 +0300

    feat/message-queue update WORKDAYS to include Friday

--- src/server/api/message-queue/message-queue.constants.ts
+++ src/server/api/message-queue/message-queue.constants.ts
@@ -1,4 +1,4 @@
-export const WORKDAYS = [0, 1, 2, 3, 4]; // Sunday to Thursday (0=Sunday, 1=Monday, etc.)
+export const WORKDAYS = [0, 1, 2, 3, 4, 5]; // Sunday to Friday (0=Sunday, 1=Monday, etc.)
 export const WORKHOURS = [9, 17]; // 9 AM to 5 PM
 export const TIMEZONE = 'Asia/Jerusalem'; // Jerusalem timezone
 export const MAX_SEND_ATTEMPT = 3; // Maximum number of attempts to send a message
